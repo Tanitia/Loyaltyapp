55bf632dc08b12658b18106d6a7484d219ee0048
T <45005624+Tanitia@users.noreply.github.com>
2022-06-24 15:23:07 +0100

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿
+namespace Loyaltyapp
+{
+    partial class AppLogin
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AppLogin));
+            this.logoBox = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.emailLabel = new System.Windows.Forms.Label();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.emailTextbox = new System.Windows.Forms.TextBox();
+            this.passwordTextbox = new System.Windows.Forms.TextBox();
+            this.signinButton = new System.Windows.Forms.Button();
+            this.registerLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.logoBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // logoBox
+            // 
+            this.logoBox.Image = ((System.Drawing.Image)(resources.GetObject("logoBox.Image")));
+            this.logoBox.Location = new System.Drawing.Point(122, 12);
+            this.logoBox.Name = "logoBox";
+            this.logoBox.Size = new System.Drawing.Size(125, 41);
+            this.logoBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.logoBox.TabIndex = 0;
+            this.logoBox.TabStop = false;
+            this.logoBox.Click += new System.EventHandler(this.logoBox_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(153, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Loyalty";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // emailLabel
+            // 
+            this.emailLabel.AutoSize = true;
+            this.emailLabel.Location = new System.Drawing.Point(73, 115);
+            this.emailLabel.Name = "emailLabel";
+            this.emailLabel.Size = new System.Drawing.Size(46, 20);
+            this.emailLabel.TabIndex = 2;
+            this.emailLabel.Text = "Email";
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Location = new System.Drawing.Point(73, 213);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(71, 20);
+            this.passwordLabel.TabIndex = 3;
+            this.passwordLabel.Text = "Password";
+            // 
+            // emailTextbox
+            // 
+            this.emailTextbox.BackColor = System.Drawing.SystemColors.WindowText;
+            this.emailTextbox.ForeColor = System.Drawing.SystemColors.Window;
+            this.emailTextbox.Location = new System.Drawing.Point(73, 147);
+            this.emailTextbox.Name = "emailTextbox";
+            this.emailTextbox.Size = new System.Drawing.Size(231, 27);
+            this.emailTextbox.TabIndex = 4;
+            // 
+            // passwordTextbox
+            // 
+            this.passwordTextbox.BackColor = System.Drawing.SystemColors.WindowText;
+            this.passwordTextbox.ForeColor = System.Drawing.SystemColors.Window;
+            this.passwordTextbox.Location = new System.Drawing.Point(73, 246);
+            this.passwordTextbox.Name = "passwordTextbox";
+            this.passwordTextbox.Size = new System.Drawing.Size(231, 27);
+            this.passwordTextbox.TabIndex = 5;
+            // 
+            // signinButton
+            // 
+            this.signinButton.Location = new System.Drawing.Point(153, 351);
+            this.signinButton.Name = "signinButton";
+            this.signinButton.Size = new System.Drawing.Size(94, 29);
+            this.signinButton.TabIndex = 6;
+            this.signinButton.Text = "Sign-in";
+            this.signinButton.UseVisualStyleBackColor = true;
+            // 
+            // registerLabel
+            // 
+            this.registerLabel.AutoSize = true;
+            this.registerLabel.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.registerLabel.Location = new System.Drawing.Point(164, 419);
+            this.registerLabel.Name = "registerLabel";
+            this.registerLabel.Size = new System.Drawing.Size(63, 20);
+            this.registerLabel.TabIndex = 7;
+            this.registerLabel.Text = "Register";
+            // 
+            // AppLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(377, 501);
+            this.Controls.Add(this.registerLabel);
+            this.Controls.Add(this.signinButton);
+            this.Controls.Add(this.passwordTextbox);
+            this.Controls.Add(this.emailTextbox);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.emailLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.logoBox);
+            this.Name = "AppLogin";
+            this.Text = "GAME Loyalty";
+            this.TopMost = true;
+            ((System.ComponentModel.ISupportInitialize)(this.logoBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox logoBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label emailLabel;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.TextBox emailTextbox;
+        private System.Windows.Forms.TextBox passwordTextbox;
+        private System.Windows.Forms.Button signinButton;
+        private System.Windows.Forms.Label registerLabel;
+    }
+}
+
